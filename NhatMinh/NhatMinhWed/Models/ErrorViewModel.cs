a86ac3447e5a6c03d31b8042d3a7a26dd35bf080
nhatnl <nhattrang815@gmail.com>
2026-08-25 22:00:39 +0700

    nhatnl-updated

--- NhatMinh/NhatMinhWed/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace NhatMinhWed.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
